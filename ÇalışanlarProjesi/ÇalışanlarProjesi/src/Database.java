d8abce6aec5bff11282d735d3566241c858e248c
Elber Çalışkan <elbr331@gmail.com>
2025-12-28 15:24:10 +0300

    Şirket Otomasyonu
    çalışan ekleme çıkarma maaş ekleme

---
+++ ÇalışanlarProjesi/ÇalışanlarProjesi/src/Database.java
@@ -0,0 +1,13 @@
+
+public class Database {
+    
+    public static final String kullanici_adi = "root";
+    public static final String parola = "123456789";
+    
+    public static final String db_ismi = "demo";
+    
+    public static final String host =  "localhost";
+    
+    public static final int port = 3306;
+    
+}
